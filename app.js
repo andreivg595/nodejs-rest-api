dc1f23d1a8c9a41cb1190096d858170322b82822
andreivg <andreivg595@gmail.com>
2023-05-08 12:08:52 +0200

    Get post and ajustments for get all posts

--- app.js
+++ app.js
@@ -1,15 +1,44 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, uuidv4())
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg'
+    ) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
 const MONGODB_URI = 'mongodb+srv://andreivg595:4wwHcMwu4BslQ8UQ@cluster0.3c2jht1.mongodb.net/messages?retryWrites=true&w=majority';
 
 //app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
+app.use(multer({
+    storage: fileStorage,
+    fileFilter: fileFilter}).single('image')
+);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -19,6 +48,15 @@ app.use((req, res, next) => {
 })
 
 app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({
+        message: message
+    });
+});
 
 mongoose
     .connect(MONGODB_URI)
