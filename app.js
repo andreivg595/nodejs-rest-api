7bf21b084140a313fb80ff9bc4148ab5e5b1c8dd
andreivg <andreivg595@gmail.com>
2023-05-08 09:22:55 +0200

    Added mongoose, create new post

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
+
+const MONGODB_URI = 'mongodb+srv://andreivg595:4wwHcMwu4BslQ8UQ@cluster0.3c2jht1.mongodb.net/messages?retryWrites=true&w=majority';
 
 //app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
@@ -17,4 +20,9 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose
+    .connect(MONGODB_URI)
+    .then(result => {
+        app.listen(8080);
+    })
+    .catch(err => console.log(err));
